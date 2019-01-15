5971e4ee37f0cab69135fec7396722e3a457b18b
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2019-01-15 11:07:14 +0100

    Add project files.

---
+++ generic_solution/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic_solution
+{
+    public class DataSample<T>
+    {
+        private T theData;
+
+        public DataSample(T theData)
+        { //test
+            this.theData = theData;
+        }
+        public T GetData()
+        {
+            return theData;
+        }
+        public string MyToString()
+        {
+            return theData.ToString();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        DataSample<string> dss = new DataSample<string>("forty-two");
+        DataSample<int> dsi = new DataSample<int>(42);
+
+        string s = dss.GetData();
+        int i = dsi.GetData();
+
+            Console.WriteLine(string.Format("s={0}, i={1}"));
+            Console.ReadKey();
+        }
+    }
+}
